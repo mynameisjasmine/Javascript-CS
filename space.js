f0d5fbba6b8a57fce6016af48061a74326630b95
Jasmine <jasminecharles@hotmail.com>
2020-07-27 15:17:58 -0700

    space complexity

---
+++ space.js
@@ -0,0 +1,5 @@
+// Most primitives (booleans, numbers, undefined, null) are constant space
+
+// Strings require O(n) space (where n is the string length)
+
+// Reference types are generally O(n) where n is the length (for arrays) or the number of keys (for objects)
