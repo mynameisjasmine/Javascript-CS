126debacd2cd2dc7f5763eaa184569020e8138a5
Jasmine <jasminecharles@hotmail.com>
2020-08-04 16:35:50 -0700

    refactoring

--- problemSolving.js
+++ problemSolving.js
@@ -30,9 +30,12 @@
 // Take the actual steps of the problem and write them down
 
 
+
+
+
 // 4. Solve/ Simplify
 
-// Solve the problem if you can...if you can't solve a simpler problem
+// Solve the problem if you can...if you can't,   solve a simpler problem
 
 /* 
 1. Find the core difficulty in what you're trying to do
@@ -42,3 +45,21 @@
 
 
 */
+
+
+
+
+// 5. Look Back and Refactor
+
+//  Even if your solution already works, it's always good to go back to your code and look for ways to make it better
+
+/*
+1. Can you check the result?
+2. Can you derive the result differently?
+3. Can you understand it at a glance?
+4. Can you use the result or method for some other problem?
+5. Can you improve the performance of your solution?
+6. Can you think of other ways to refactor?
+7. How have other people solved this problem? 
+*/
+
